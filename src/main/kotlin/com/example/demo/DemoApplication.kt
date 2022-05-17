ca7ca82cc19645b896e2d10e670abd28d4f1f804
taneli1 <meikalainen31@gmail.com>
2022-05-18 00:03:00 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,23 @@
+package com.example.demo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class DemoApplication {
+	@GetMapping("/hello")
+	fun hello(@RequestParam(value = "name", defaultValue = "World") name: String?): String {
+		return String.format("Hello %s!", name)
+	}
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			SpringApplication.run(DemoApplication::class.java, *args)
+		}
+	}
+}
